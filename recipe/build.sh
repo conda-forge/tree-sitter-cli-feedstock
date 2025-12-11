eabb9d34b0f7b1b80c9589f77da829fb26914c02
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-10 16:30:44 -0800

    fix path

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,4 +14,4 @@ cargo-bundle-licenses \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-cargo install --bins --no-track --locked --root ${PREFIX} --path cli
+cargo install --bins --no-track --locked --root ${PREFIX} --path crates/cli
