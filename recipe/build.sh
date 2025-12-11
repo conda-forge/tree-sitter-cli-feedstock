edc26f45a069bac29139a666e8cba0ea9ec4a3a8
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-10 16:49:55 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
+export LIBCLANG_PATH="${BUILD_PREFIX}/lib"
 
 # Add CFLAG here because it cannot be added any other way.
 sed -i 's/.define("_DEFAULT_SOURCE", None)/.define("_DEFAULT_SOURCE", None).define("_BSD_SOURCE", None)/' lib/binding_rust/build.rs
